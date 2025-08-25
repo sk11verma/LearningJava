6036fecce0001d2f3b586272e52f7790ff92fd23
Suresh <sk11verma@gmail.com>
2025-08-26 01:52:43 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.gitignore
    #	README.md

---
+++ src/JavaBasics/Second_Comments.java
@@ -0,0 +1,15 @@
+package src.JavaBasics;
+
+public class Second_Comments {
+    public static void main(String[] args) {
+        //this is single line comment
+        System.out.println("Hello single comment");
+
+        /*
+        Multiline comments
+        hello////
+         */
+        System.out.println("Multipline comments");
+
+    }
+}
