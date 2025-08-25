56acfedb6a6a8c4ccd20b6fc89ae6170662b1284
Suresh <sk11verma@gmail.com>
2025-08-26 02:12:02 +0530

    commit

---
+++ src/JavaBasics/Comments.java
@@ -0,0 +1,12 @@
+package JavaBasics;
+
+public class Comments {
+    public static void main(String[] args) {
+        //System.out.println("with single comment");
+        System.out.println("No comment");
+        /*
+        System.out.println("Multiline Comment");
+         */
+        System.out.println("No comment");
+    }
+}
---
+++ src/JavaBasics/HelloJava.java
@@ -0,0 +1,7 @@
+package JavaBasics;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello first Java Program");
+    }
+}
